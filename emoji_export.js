3563725a6b8f42e005be67f01c17623296697eaf
Lukas Breuer <lukas.breuer@outlook.com>
2017-11-13 16:11:01 +0100

    Fix script formatting

--- emoji_export.js
+++ emoji_export.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 // Read token
 let token;
 if (process.argv[0].match(/.*node.*/) !== null) {
-	token = process.argv[2];
+  token = process.argv[2];
 } else {
-	token = process.argv[1];
+  token = process.argv[1];
 }
 
 // Connect to discord
@@ -14,75 +14,75 @@ const ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=6');
 
 // When the socket opens, pretend to be the discord client
 ws.on('open', () => {
-	// Send the login packet
-	ws.send(JSON.stringify({
-		op: 2,
-		d: {
-			token,
-			properties: {
-				os: "Mac OS X",
-				browser: "Chrome",
-				device: "",
-				referrer: "",
-				referring_domain: ""
-			},
-			large_threshold: 100,
-			synced_guilds: [],
-			presence: {
-				status: "online",
-				since: 0,
-				afk: false,
-				game: null
-			},
-			compress: false
-		}
-	}));
+  // Send the login packet
+  ws.send(JSON.stringify({
+    op: 2,
+    d: {
+      token,
+      properties: {
+        os: "Mac OS X",
+        browser: "Chrome",
+        device: "",
+        referrer: "",
+        referring_domain: ""
+      },
+      large_threshold: 100,
+      synced_guilds: [],
+      presence: {
+        status: "online",
+        since: 0,
+        afk: false,
+        game: null
+      },
+      compress: false
+    }
+  }));
 
-	// Send the state packet
-	ws.send(JSON.stringify({
-		op: 4,
-		d: {
-			guild_id: null,
-			channel_id: null,
-			self_mute: true,
-			self_deaf: true,
-			self_video: false
-		}
-	}));
+  // Send the state packet
+  ws.send(JSON.stringify({
+    op: 4,
+    d: {
+      guild_id: null,
+      channel_id: null,
+      self_mute: true,
+      self_deaf: true,
+      self_video: false
+    }
+  }));
 });
 
 // Process the initial state packet
 ws.on('message', data => {
-	// Parse answer
-	data = JSON.parse(data);
+  // Parse answer
+  data = JSON.parse(data);
 
-	// Log event
-	console.log('[W] ' + data['op'] + " / " + data['t']);
+  // Log event
+  console.log('[W] ' + data['op'] + " / " + data['t']);
 
-	// Drop event if not READY
-	if (data.op !== 0 || data.t !== "READY") {
-		return;
-	}
+  // Drop event if not READY
+  if (data.op !== 0 || data.t !== "READY") {
+    return;
+  }
 
-	// We've got what we wanted.
-	ws.close();
+  // We've got what we wanted.
+  ws.close();
 
-	// Aggregate emojis
-	let emojis = {};
+  // Aggregate emojis
+  let emojis = {};
 
-	for(let guild of data.d.guilds) {
-		console.log();
-		console.log('[G] ' + guild.id + ' / ' + guild.name);
+  for (let guild of data.d.guilds) {
+    console.log();
+    console.log('[G] ' + guild.id + ' / ' + guild.name);
 
-		for(let emoji of guild.emojis) {
-			console.log('[E] ' + emoji.name + ' -> ' + emoji.id);
-			emojis[emoji.name] = emoji.id;
-		}
-	}
+    for (let emoji of guild.emojis) {
+      console.log('[E] ' + emoji.name + ' -> ' + emoji.id);
+      emojis[emoji.name] = emoji.id;
+    }
+  }
 
-	// Save emojis as json
-	fs.writeFileSync('./emojis.json', JSON.stringify(emojis));
+  // Save emojis as json
+  fs.writeFileSync('./emojis.json', JSON.stringify(emojis));
 
-	// Exit
-	process.exit(0);
+  // Exit
+  process.exit(0);
 });
--- emoji_format.js
+++ emoji_format.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 const emojis = JSON.parse(
-	fs.readFileSync('./emojis.json')
+  fs.readFileSync('./emojis.json')
 );
 
-let md = 
-	"| Name | Emoji | Name | Emoji | Name | Emoji |\n" +
-	"|:-----|:-----:|:----:|:-----:|:----:|:-----:|\n"
-;
+let md =
+  "| Name | Emoji | Name | Emoji |\n" +
+  "|:----:|:-----:|:----:|:-----:|\n";
 
 let tmp = [];
 for (let k in emojis) {
-	if (tmp.length < 5) {
-		tmp.push(`\`:${k}:\``);
-		tmp.push(`![](https://cdn.discordapp.com/emojis/${emojis[k]}.png)`);
-		continue;
-	}
+  if (tmp.length < 3) {
+    tmp.push(`\`:${k}:\``);
+    tmp.push(`![](https://cdn.discordapp.com/emojis/${emojis[k]}.png)`);
+    continue;
+  }
 
-	md += tmp.join("|") + "|\n";
-	tmp = [];
+  md += tmp.join("|") + "|\n";
+  tmp = [];
 }
 
-console.log(md);
+console.log(
+  `Listing ${Object.keys(emojis).length} emojis :o\n\n` +
+  md
+);
